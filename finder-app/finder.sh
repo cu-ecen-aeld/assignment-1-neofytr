53cf197b760aebe8d17d1faa79e814d3ffa0dc28
neofytr <imagination12344321.com@gmail.com>
2025-06-11 14:54:49 +0530

    finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,20 @@
+if [ $# -le 1 ]
+then
+    echo "needs two parameters!"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ]
+then 
+    echo "${filesdir} is not a directory on the filesystem!"
+    exit 1
+fi
+
+X=$(find . -type f | wc -l)
+Y=$(grep -r $searchstr $filesdir | wc -l)
+
+echo "The number of files are ${X} and the number of matching lines are ${Y}"
+exit 0
